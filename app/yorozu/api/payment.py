2d3c9e28a34b0052e4273d7a3abd6edda2a9b6ac
toshi <etoshi0228@gmail.com>
2020-09-20 15:34:20 +0900

    クレジットカードのpatch処理を追加

--- app/yorozu/api/payment.py
+++ app/yorozu/api/payment.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 import os
 import stripe
 from rest_framework import views, status
@@ -42,23 +43,36 @@ class PaymentAPIView(views.APIView):
 
 
 class CreatePaymentCustomer(views.APIView):
-    """決済の処理をしたユーザーの登録"""
+    """決済の処理をしたユーザーの登録、編集"""
 
     # これがあるだけで、JWTがなければエラーになる
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
+        """stripeから発行された顧客情報を取得する"""
         stripe.api_key = os.environ.get('STRIPE_KEY')
-        print("---------------")
+
         # request.data["id"]より、request.data.get("id")の方が良い request.data["id"]だとidが渡って来ない時にエラーになる
         # request.data.get("id")であれば、もしIDがない場合にNoneを返す
-        print(request.data.get("id"))
 
-        a = stripe.Customer.retrieve(request.data.get("id"))
-        print("---------------")
-        print(a)
+        try:
+            # tokenがある場合、self.request.userでユーザー情報を取り出すことができる
+            app_id = self.request.user.profile.yorozu_id
 
-        return Response("stripe処理")
+            # app_idに紐づいている、strpeから発行されたtokenを取り出す
+            queryset = Payment.objects.filter(app_id=app_id).first()
+
+            # querysetはリスト(iterable)の場合は、引数にmany=Trueが必要
+            serializer = PaymentSerializer(instance=queryset)
+
+            # stripeの処理 カード情報から顧客情報を取得する
+            customer = stripe.PaymentMethod.retrieve(
+                serializer.data["payment_method_id"]
+            )
+
+            return Response(customer, status=status.HTTP_200_OK)
+        except:
+            return Response("error", status=HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request):
         """顧客の作成"""
@@ -89,9 +103,61 @@ class CreatePaymentCustomer(views.APIView):
         serializer = PaymentSerializer(data=stripe_info)
         if serializer.is_valid():
             serializer.save()
+
+            # returnととしては、カード情報と顧客情報を紐づいたもの取得するために、retrieveを使う
+            customer = stripe.PaymentMethod.retrieve(
+                serializer.data["payment_method_id"]
+            )
+
             return Response(customer, status=status.HTTP_201_CREATED)
         else:
             return Response("エラー")
 
         # stripe.Customer.createで,metadataでuserIdを送ることで、stripe側で重複判定の処理を行うことができ、重複を防ぐことができる
         # metadata={"user_id": request.data['id']}
+
+    def patch(self, request):
+        """顧客情報の更新"""
+
+        request.data["prev_payment_method_id"]
+
+        stripe.api_key = os.environ.get('STRIPE_KEY')
+
+        # カード情報と顧客情報と切り離す
+        customer = stripe.PaymentMethod.detach(
+            request.data["prev_payment_method_id"],
+        )
+
+        # 新しいカード情報と顧客情報を紐づける
+        customer = stripe.PaymentMethod.attach(
+            request.data["next_payment_method_id"],
+            customer=request.data["customer_id"],
+        )
+
+        # 以下からは、サーバーサイドで、stripeから発行された顧客情報とカード情報のトークンを更新する処理
+        app_id = self.request.user.profile.yorozu_id
+        stripe_info = {
+            "customer_id": customer["customer"],
+            'payment_method_id': customer['id'],
+            "app_id": app_id,
+        }
+
+        # payment_method_idが同じオブジェクトを取得(過去のものを更新するために古いprev_payment_method_idを使う)
+        payment = get_object_or_404(
+            Payment, payment_method_id=request.data["prev_payment_method_id"])
+
+        # シリアライザーの初期値と、更新したいデータ、partial=Trueの3点セットで値が部分的に更新される
+        serializer = PaymentSerializer(
+            instance=payment, data=stripe_info, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            # returnととしては、カード情報と顧客情報を紐づいたもの取得するために、retrieveを使う
+            customer = stripe.PaymentMethod.retrieve(
+                serializer.data["payment_method_id"]
+            )
+
+            return Response(customer, status=status.HTTP_200_OK)
+        else:
+            return Response("エラー")
--- app/yorozu/serializers/serializer_payment.py
+++ app/yorozu/serializers/serializer_payment.py
@@ -9,19 +9,3 @@ class PaymentSerializer(serializers.ModelSerializer):
         model = Payment
         # fields = ('id', 'price')
         fields = "__all__"
-
-
-# from rest_framework import serializers
-# from ..models import Message, Profile
-# from .serializer_profile import ProfileSerializer
-
-
-# class MessageSerializer(serializers.ModelSerializer):
-
-#     sender_profile = serializers.SerializerMethodField()
-
-#     class Meta:
-#         model = Message
-
-#         fields = ("id", "sender_yorozu_id", "receiver_yorozu_id",
-#                   "message_content", "sender_profile", "unread", "created_at", "updated_at")
